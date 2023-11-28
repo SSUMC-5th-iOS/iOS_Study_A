c237d6923bc7ee57fb2ff14ee56941bd4a79c0d2
00yhsp <00yhsp@naver.com>
2023-11-29 03:22:23 +0900

    week8

---
+++ week8/week8/week8App.swift
@@ -0,0 +1,17 @@
+//
+//  week8App.swift
+//  week8
+//
+//  Created by 박현수 on 2023/11/28.
+//
+
+import SwiftUI
+
+@main
+struct week8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
